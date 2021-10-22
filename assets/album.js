46c926c17d666ffaf21ad0aba7be39a128d65257
Camila <ucami98@gmail.com>
2021-10-21 21:18:47 -0500

    first commit

---
+++ assets/album.js
@@ -0,0 +1,149 @@
+document.getElementById("b1").addEventListener("click", function () {
+  alert("Libro: El Hobbit - Año de publicación: 1937");
+});
+
+document.getElementById("b2").addEventListener("click", function () {
+  alert(
+    "Libro: El Señor de Los Anillos: La comunidad del Anillo - Año de publicación: 1954"
+  );
+});
+
+document.getElementById("b3").addEventListener("click", function () {
+  alert(
+    "Libro: El Señor de Los Anillos: Las Dos Torres - Año de publicación: 1954"
+  );
+});
+
+document.getElementById("b4").addEventListener("click", function () {
+    alert(
+      "Libro: El Señor de Los Anillos: El Retorno del Rey - Año de publicación: 1955"
+    );
+  });
+
+  document.getElementById("b5").addEventListener("click", function () {
+    alert(
+      "Libro: El Silmarillion - Año de publicación: 1977"
+    );
+  });
+
+  document.getElementById("b6").addEventListener("click", function () {
+    alert(
+      "Libro: Cuentos Inconclusos - Año de publicación: 1980"
+    );
+  });
+
+  document.getElementById("b7").addEventListener("click", function () {
+    alert(
+      "Libro: Los Hijos de Hurin - Año de publicación: 2007"
+    );
+  });
+
+  document.getElementById("b8").addEventListener("click", function () {
+    alert(
+      "Libro: Beren y Luthien - Año de publicación: 2017"
+    );
+  });
+
+  document.getElementById("b9").addEventListener("click", function () {
+    alert(
+      "Libro: La Caida De Gondolin - Año de publicación: 2018"
+    );
+  });
+
+  document.getElementById("b10").addEventListener("click", function () {
+    alert(
+      "Libro: Las Aventuras de Tom Bolbadil - Año de publicación: 1962"
+    );
+  });
+
+  document.getElementById("b11").addEventListener("click", function () {
+    alert(
+      "Libro: La Ultima Cancion De Bilbo - Año de publicación: 1990"
+    );
+  });
+  document.getElementById("b12").addEventListener("click", function () {
+    alert(
+      "Libro: El Libro De Los Cuentos Perdidos 1 - Año de publicación: 1983"
+    );
+  });
+
+  document.getElementById("b13").addEventListener("click", function () {
+    alert(
+      "Libro: El Libro De Los Cuentos Perdidos 2 - Año de publicación: 1983"
+    );
+  });
+
+  document.getElementById("b14").addEventListener("click", function () {
+    alert(
+      "Libro: Las Baladas De Beleriand - Año de publicación: 1985"
+    );
+  });
+
+  document.getElementById("b15").addEventListener("click", function () {
+    alert(
+      "Libro: Formacion De La Tierra Media - Año de publicación: 1986"
+    );
+  });
+
+  document.getElementById("b16").addEventListener("click", function () {
+    alert(
+      "Libro: El Camino Perdido - Año de publicación: 1987"
+    );
+  });
+
+  document.getElementById("b17").addEventListener("click", function () {
+    alert(
+      "Libro: La Caida De Numenor - Año de publicación: 1992"
+    );
+  });
+  document.getElementById("b18").addEventListener("click", function () {
+    alert(
+      "Libro: El Anillo De Morgoth - Año de publicación: 1993"
+    );
+  });
+
+  document.getElementById("b19").addEventListener("click", function () {
+    alert(
+      "Libro: La Guerra De Las Joyas - Año de publicación: 1994"
+    );
+  });
+  document.getElementById("b20").addEventListener("click", function () {
+    alert(
+      "Libro: Los Pueblos De La Tierra Media - Año de publicación: 1996"
+    );
+  });
+  document.getElementById("b21").addEventListener("click", function () {
+    alert(
+      "Libro: El Retorno De La Sombra - Año de publicación: 1988"
+    );
+  });
+  document.getElementById("b22").addEventListener("click", function () {
+    alert(
+      "Libro: La Traicion De Isengard - Año de publicación: 1989"
+    );
+  });
+  document.getElementById("b23").addEventListener("click", function () {
+    alert(
+      "Libro: La Guerra Del Anillo - Año de publicación: 1990"
+    );
+  });
+  document.getElementById("b24").addEventListener("click", function () {
+    alert(
+      "Libro: El Fin De La Tercera Edad - Año de publicación: 1992"
+    );
+  });
+  document.getElementById("b25").addEventListener("click", function () {
+    alert(
+      "Libro: Cuentos Desde El Reino Peligroso - Año de publicación: 1997"
+    );
+  });
+  document.getElementById("b26").addEventListener("click", function () {
+    alert(
+      "Libro: La Caida de Arturo - Año de publicación: 2013"
+    );
+  });
+  document.getElementById("b27").addEventListener("click", function () {
+    alert(
+      "Libro: La Leyenda De Sigurd Y Gudrun - Año de publicación: 2009"
+    );
+  });
